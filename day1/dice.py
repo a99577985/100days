ddc0e0a4bbf243ec54c0287c4a19b277106926ec
xiaoyz <a99577985@163.com>
2019-06-25 00:03:01 +0800

    625

---
+++ day1/dice.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+'''
+# Created on 6月-24-19 21:56
+# dice.py
+# @author: xyz
+'''
+from random import randint
+i = randint(1,6)
+if i==1:
+    print('sing')
+elif i==2:
+    print('dance')
+elif i== 3:
+    print('rap')
+elif i == 4:
+    print('cai')
+elif i == 5:
+    print('noob')
+else:
+    print('burden')
+print(i)
---
+++ day1/scores.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+'''
+# Created on 6月-24-19 23:21
+# scores.py
+# @author: xyz
+'''
+score = int(input('输入成绩：'))
+if score>=100 or score<0:
+    level='你吓唬谁呢'
+elif score>=90 and score<=100:
+    level='A'
+elif score>=80:
+    level='B'
+elif score>=70:
+    level='C'
+elif score>=60:
+    level='D'
+elif score>=0:
+    level='E'
+print('你的成绩是%s'%(level))    
