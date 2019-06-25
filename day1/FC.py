0774cea3b3aa6b7d7e7440aff090f87d989fdc27
a99577985 <xyz_xiao@qq.com>
2019-06-25 14:42:41 +0800

    625

--- day1/FC.py
+++ day1/FC.py
@@ -1,4 +1,10 @@
+# -*- coding: utf-8 -*-
+'''
+# Created on 6月-25-19 10:32
+# FC.py
+# @author: xyz
+'''
 f = float(input('请输入华氏温度：'))
-c = (f -32)/1.8
-print('%.1f华氏度 = %.1f摄氏度'%(f,c))
-#print('%.1f华氏度 = %.1f摄氏度' %(f, c))
+c = (f - 32) / 1.8
+print('%.1f华氏度 = %.1f摄氏度' % (f, c))
+# print('%.1f华氏度 = %.1f摄氏度' %(f, c))
--- day1/scores.py
+++ day1/scores.py
@@ -5,16 +5,16 @@
 # @author: xyz
 '''
 score = int(input('输入成绩：'))
-if score>=100 or score<0:
-    level='你吓唬谁呢'
-elif score>=90 and score<=100:
-    level='A'
-elif score>=80:
-    level='B'
-elif score>=70:
-    level='C'
-elif score>=60:
-    level='D'
-elif score>=0:
-    level='E'
-print('你的成绩是%s'%(level))    
+if score >= 100 or score < 0:
+    level = '你吓唬谁呢'
+elif score >= 90 and score <= 100:
+    level = 'A'
+elif score >= 80:
+    level = 'B'
+elif score >= 70:
+    level = 'C'
+elif score >= 60:
+    level = 'D'
+elif score >= 0:
+    level = 'E'
+print('你的成绩是:%s' % (level))
